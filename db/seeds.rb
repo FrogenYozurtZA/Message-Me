d748c5e7af980b4be854f13965eea0cebaf903b5
River van Blerk <rivervanblerk@gmail.com>
2019-12-14 16:23:55 +0200

    Add users and messages models and db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Alexa", password: "password2")
+User.create(username: "Tom", password: "password3")
+User.create(username: "Mike",password: "password4")
+User.create(username: "Mia", password: "password5")
+User.create(username: "Alice", password: "password6")
+User.create(username: "Scarlet", password: "password7")
+
+Message.create(body: "Hey Tom, whats up?", user_id: 2)
+Message.create(body: "Hey Alexa!", user_id: 3)
+Message.create(body: "Chillin like a villan. You?", user_id: 3)
+Message.create(body: "Sounds fun! Yeah pretty much the same.", user_id: 2)
